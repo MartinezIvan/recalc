e06a2b5d6358e98914508fdf47784a15654c5c8b
Ivan Martinez <martinezezequielivan@gmail.com>
2023-04-25 20:10:10 -0300

    test: Agregando tests para la funcion addition

--- test/core.test.js
+++ test/core.test.js
@@ -9,3 +9,19 @@ describe('Subtract', () => {
         expect(core.sub(6, 4)).toBe(2); 
     })
 })
+
+describe('Addition', () => {
+    test('Deberia 0 + 2 = 2', () => {
+        expect(core.add(0, 2)).toBe(2); 
+    })
+
+    test('Deberia 6 + 4 = 10', () => {
+        expect(core.add(6, 4)).toBe(10); 
+    })
+    test('Deberia -4 + 4 = 0', () => {
+        expect(core.add(-4, 4)).toBe(0); 
+    })
+    test('Deberia -4 + 8 = 0', () => {
+        expect(core.add(-4, 8)).toBe(4); 
+    })
+})
