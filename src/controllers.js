8ccdc2343c5f8c594dd4cb6bfa5ea9de6da78500
MondinoPedro <mondinopedro12@gmail.com>
2023-05-29 18:07:08 -0300

    Saving pow endpoint

--- src/controllers.js
+++ src/controllers.js
@@ -52,6 +52,7 @@ router.get("/pow/:a", async function (req, res) {
     if (isNaN(a))
         return res.status(400).send('El parametro debe ser un numero.');
     const result = core.pow(a);
+    await createHistoryEntry({firstArg: a, operationName:"POW"})
     return res.send({result})
 });
 
